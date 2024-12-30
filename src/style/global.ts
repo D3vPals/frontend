cb3ce91fb228339b503956cbf1ee32ab2c3421e9
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-12-30 19:06:12 +0900

    [chore] 프로젝트 초기 환경 설정

---
+++ src/style/global.ts
@@ -0,0 +1,48 @@
+import 'sanitize.css';
+
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    font-family: 'Pretendard';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    box-sizing: border-box;
+  }
+  html {
+    font-family: 'Pretendard';
+  }
+  body {
+    font-family: 'Pretendard';
+  }
+  h1 {
+    margin: 0;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    border: 0;
+    &:focus {
+      box-shadow: none;
+      outline: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    padding-left: 0;
+    list-style: none;
+  }
+`;
