dc63452669df6d03097b1ec5c2bac712fabd20ca
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-01-03 01:06:42 +0900

    feat : 모집공고 작성 페이지 기본 폼 생성

--- src/pages/createProject/CreateProject.styled.ts
+++ src/pages/createProject/CreateProject.styled.ts
@@ -1,3 +1,148 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 60px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.color.border};
+  border-radius: ${({ theme }) => theme.borderRadius};
+`;
+
+export const Title = styled.h1`
+  font-size: ${({ theme }) => theme.heading.medium};
+  font-weight: bold;
+  margin-bottom: 50px;
+`;
+
+export const DateContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const DateInput = styled.input`
+  flex: none;
+  padding: 10px 9px;
+  border: 1px solid ${({ theme }) => theme.color.border};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  font-size: ${({ theme }) => theme.heading.small};
+  background-color: #ffffff;
+  color: #aaa;
+  font-family: 'Arial', sans-serif;
+
+  &::placeholder {
+    color: #aaa;
+  }
+
+  input[type='date']::-webkit-datetime-edit-text {
+    -webkit-appearance: none;
+    display: none;
+  }
+  input[type='date']::-webkit-datetime-edit-month-field {
+    -webkit-appearance: none;
+    display: none;
+  }
+  input[type='date']::-webkit-datetime-edit-day-field {
+    -webkit-appearance: none;
+    display: none;
+  }
+  input[type='date']::-webkit-datetime-edit-year-field {
+    -webkit-appearance: none;
+    display: none;
+  }
+`;
+
+export const Separator = styled.span`
+  font-size: ${({ theme }) => theme.heading.small};
+  color: #ccc;
+`;
+
+export const SectionTitle = styled.label`
+  display: block;
+  font-size: ${({ theme }) => theme.heading.small};
+  font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+export const Section = styled.div`
+  margin-bottom: 30px;
+`;
+
+export const SectionInput = styled.div`
+  margin-bottom: 40px;
+  padding: 20px;
+  border: 1px solid ${({ theme }) => theme.color.border};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  background-color: #fff;
+`;
+
+export const InfoRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 15px;
+
+  label {
+    font-size: 0.8rem;
+    font-weight: bold;
+    color: #333;
+    flex: 0.08;
+  }
+
+  input {
+    flex: 0.3;
+    border: none;
+    font-size: 0.8rem;
+  }
+  ,
+  select {
+    flex: 0.6;
+    padding: 10px;
+    border: 1px solid ${({ theme }) => theme.color.border};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    font-size: 0.5rem;
+    background-color: #f9f9f9;
+
+    &::placeholder {
+      color: #aaa;
+    }
+  }
+`;
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 3px;
+`;
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  height: 150px;
+  padding: 12px;
+  margin-top: 5px;
+  border: 1px solid ${({ theme }) => theme.color.border};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  font-size: ${({ theme }) => theme.heading.small};
+  resize: none;
+`;
+
+export const SubmitButton = styled.button`
+  width: 10%;
+  padding: 15px;
+  background-color: ${({ theme }) => theme.buttonScheme.primary.bg};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.heading.small};
+  font-weight: bold;
+  border: none;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
--- src/pages/createProject/CreateProject.tsx
+++ src/pages/createProject/CreateProject.tsx
@@ -2,11 +2,113 @@ import React from 'react';
 import * as S from './CreateProject.styled';
 
 const CreateProject = () => {
-  /*
-  모집공고 작성 페이지
-  */
+  const projectData = [
+    {
+      name: 'recruitment',
+      label: '모집 인원',
+      type: 'text',
+      placeholder: '인원을 적어주세요.',
+    },
+    {
+      name: 'startDate',
+      label: '시작 예정',
+      type: 'text',
+      placeholder: 'YYYY. MM. DD',
+    },
+    {
+      name: 'field',
+      label: '모집 분야',
+      type: 'text',
+      placeholder: '모집 분야를 적어주세요.',
+    },
+    {
+      name: 'duration',
+      label: '예상 기간',
+      type: 'text',
+      placeholder: '예상 기간을 적어주세요. ex. 5개월.',
+    },
+    {
+      name: 'method',
+      label: '진행 방식',
+      type: 'text',
+      placeholder: '온라인 / 오프라인 / 온오프라인',
+    },
+  ];
 
-  return <S.Container>CreateProject Component</S.Container>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries()); // FormData -> Object 변환
+    console.log(data); // 제출된 데이터를 확인
+  };
+
+  return (
+    <S.Container>
+      <S.Title>프로젝트 생성</S.Title>
+
+      <form onSubmit={handleSubmit}>
+        <S.Section>
+          <S.SectionTitle>프로젝트 모집 일정</S.SectionTitle>
+          <S.DateContainer>
+            <S.DateInput name="startDate" type="date" placeholder="시작 날짜" />
+            <S.Separator>-</S.Separator>
+            <S.DateInput name="endDate" type="date" placeholder="종료 날짜" />
+          </S.DateContainer>
+        </S.Section>
+
+        <S.Section>
+          <S.SectionTitle>프로젝트 제목</S.SectionTitle>
+          <S.Input
+            name="title"
+            type="text"
+            placeholder="프로젝트 제목을 입력해주세요."
+          />
+        </S.Section>
+
+        <S.Section>
+          <S.SectionTitle>프로젝트 정보</S.SectionTitle>
+          <S.SectionInput>
+            {projectData.map((input, index) => (
+              <S.InfoRow key={index}>
+                <label htmlFor={input.name}>{input.label}</label>
+                <input
+                  id={input.name}
+                  name={input.name}
+                  type={input.type}
+                  placeholder={input.placeholder}
+                />
+              </S.InfoRow>
+            ))}
+            <S.InfoRow>
+              <label htmlFor="newMember">새싹 여부</label>
+              <S.CheckboxContainer>
+                <input id="newMember" name="newMember" type="checkbox" />
+              </S.CheckboxContainer>
+            </S.InfoRow>
+
+            <S.InfoRow>
+              <label htmlFor="languages">사용 언어</label>
+              <S.Input
+                name="languages"
+                type="text"
+                placeholder="사용 언어를 입력해주세요."
+              />
+            </S.InfoRow>
+          </S.SectionInput>
+        </S.Section>
+
+        <S.Section>
+          <S.SectionTitle>프로젝트 내용</S.SectionTitle>
+          <S.TextArea
+            name="description"
+            placeholder="프로젝트 상세 정보를 입력해주세요."
+          />
+        </S.Section>
+
+        <S.SubmitButton type="submit">제출</S.SubmitButton>
+      </form>
+    </S.Container>
+  );
 };
 
 export default CreateProject;
